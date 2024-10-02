0e2f153745b12a800ea9ff893551e9e22d4985c1
MarvinKatapult <ibingmarvin@gmail.com>
2024-10-02 21:46:14 +0200

    First working

--- main.c
+++ main.c
@@ -11,8 +11,10 @@ typedef struct {
 	unsigned char a;
 } Pixel;
 
+#define USED_CHARS  ".`;-'*|><)(][\\/+=#wxlstymjcurvzpfnqkhgedbaOoILTSPCYEJRVEUKGFDXAHQZBNMW"
+
 Pixel * preloadImage(unsigned char * img, int w, int h, int channels) {
-	Pixel * ret = calloc(w * h * channels, sizeof(Pixel));
+	Pixel * ret = calloc(w * h, sizeof(Pixel));
 	for (int y = 0; y < h; y++) {
 		for (int x = 0; x < w; x++) {
 			Pixel * new_pix = ret + (y * w + x);
@@ -28,26 +30,68 @@ Pixel * preloadImage(unsigned char * img, int w, int h, int channels) {
 	return ret;
 }
 
-int main(int argc, char **argv) {
+unsigned char getAvgBrightness(Pixel p) {
+    return (p.r + p.g + p.b) / 3;
+}
+
+unsigned char getAvgBrightnessSector(Pixel * start, int w, int size) {
+    int count = 0;
+    int abs_brightness = 0;
+    for (int y = 0; y < size; y++) {
+        for (int x = 0; x < size; x++) {
+            const Pixel * p = start + (x + y * w);
+            abs_brightness += getAvgBrightness(*p);
+            count++;
+        }
+    }
+    return abs_brightness / count;
+}
+
+char mapAvgBToChar(unsigned char b) {
+    const char * c = USED_CHARS;
+    const float f = strlen(c) / 256.f;
+    return c[(int)(f * b)];
+}
+
+int main(int argc, char * argv[]) {
 
     if (argc < 2) {
-        printf("USAGE: %s image_path\n", argv[0]);
+        printf("USAGE: %s image_path [cell_pixel_size]\n", argv[0]);
         return 1;
     }
 
     int w, h, channels;
-    unsigned char *img = stbi_load(argv[1], &w, &h, &channels, 0);
+    unsigned char * img = stbi_load(argv[1], &w, &h, &channels, 0);
     if (img == NULL) {
         printf("Error in loading the image\n");
+        return 1;
+    }
+
+    if (!strstr(argv[1], ".jpg") && !strstr(argv[1], ".png") && !strstr(argv[1], ".jpeg")) {
+        printf("Provided image is not in the right format. (.png or .jpg)\n");
         return 1;
     }
 
 	Pixel * pixels = preloadImage(img, w, h, channels);
     stbi_image_free(img);
 
-	for (int i = 0; i < w * h * channels; i++) {
-		if (i > w * h * channels - 10) printf("R:%d\n", pixels[i].r);
-	}
+    int size = 8;
+    if (argc > 2) {
+        size = atoi(argv[2]);    
+    }
+    const int count_x = w / size;
+    const int count_y = h / size;
+    for (int y = 0; y < count_y; y++) {
+        for (int x = 0; x < count_x; x++) {
+            unsigned char b = getAvgBrightnessSector(pixels + ((x + y * w) * size), w, size);
+            // printf("Avg. B for Sector:X=%d;Y=%d %d\n", x, y, b);
+            printf("%c", mapAvgBToChar(b));
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+    free (pixels);
 
     return 0;
 }
